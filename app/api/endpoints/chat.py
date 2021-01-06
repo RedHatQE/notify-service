8c5f09c2d95fb25620aab6c7c3604dd143c9627b
Wayne Sun <gsun@redhat.com>
2021-01-06 11:21:26 -0500

    Include subject in chat message
    Signed-off-by: Wayne Sun <gsun@redhat.com>

--- app/api/endpoints/chat.py
+++ app/api/endpoints/chat.py
@@ -2,7 +2,7 @@ import httpx
 
 from typing import Any, Optional, Dict
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
 from pydantic.networks import AnyHttpUrl
 
 from app.core.config import settings
@@ -15,7 +15,7 @@ router = APIRouter()
 async def send_message(
     subject: str = "",
     template_name: str = "chat_default",
-    environment: Dict[str, Any] = {"body": {}},
+    environment: Dict[str, Any] = {"body": ""},
     webhook_url: Optional[AnyHttpUrl] = None,
     template_url: Optional[AnyHttpUrl] = None
 ) -> Any:
@@ -27,10 +27,15 @@ async def send_message(
     - **template_url**: template url, optional
     """
     if "project_name" not in environment:
-        environment["project_name"] = settings.PROJECT_NAME
+        subject = "*" + settings.PROJECT_NAME + "* " + subject
+    else:
+        subject = "*" + environment["project_name"] + "* " + subject
+
+    environment["body"] = "\n".join((subject, environment["body"]))
 
     headers = {'Content-Type': 'application/json; charset=UTF-8'}
     data = await utils.get_template(template_name, template_url, '.jinja', environment)
+
 
     if webhook_url:
         url = webhook_url
@@ -39,5 +44,11 @@ async def send_message(
 
     async with httpx.AsyncClient() as client:
         r = await client.post(url, headers=headers, data=data)
+
+    if r.status_code != status.HTTP_200_OK:
+        raise HTTPException(
+            status_code=r.status_code,
+            detail=r.text,
+        )
 
     return {"msg": r.text}
