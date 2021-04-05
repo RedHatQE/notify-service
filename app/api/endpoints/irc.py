da01f6a98bf71413bd152b39fc626b0add539313
Wayne Sun <gsun@redhat.com>
2021-04-05 18:08:28 -0400

    Update request body schemas
    Add new TextBody and DictBody schema for request body, use Union for
    support multiple schemas.
    
    Update apis with the schemas, this change api behavior with 'body'
    key is required in request body.
    
    Also update examples and template url for request body.
    
    Signed-off-by: Wayne Sun <gsun@redhat.com>

--- app/api/endpoints/irc.py
+++ app/api/endpoints/irc.py
@@ -20,17 +20,21 @@ async def _send_message(message, channel) -> Any:
     except OSError as err:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"{err}. Please check IRC config is right and server:port is accessible.")
+            detail=f"{err}. Please check IRC config is right and server:port is accessible."
+        )
 
 
 @router.post("/", response_model=schemas.Msg)
 async def send_message(
     channel: str = Query(
         ...,
-        description="IRC channel name start with '#' or a user name, e.g. channel #test or user john"),
+        description="IRC channel name start with '#' or a user name, e.g. channel #test or user john"
+    ),
     message: str = Query(
         ...,
-        description="The text message")
+        description="The text message",
+        example="SAMPLE MESSAGE"
+    )
 ) -> Any:
     """
     Send message to irc channel or user
