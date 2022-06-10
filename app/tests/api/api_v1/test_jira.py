f752ce4c6497508ea57750a22e53596db5aa10c0
Ido Burstein <idoburstein@gmail.com>
2022-06-10 16:30:42 -0400

    Fixed templates names and merged jira unit tests
    Signed-off-by: Ido Burstein <idoburstein@gmail.com>

--- app/tests/api/api_v1/test_jira.py
+++ app/tests/api/api_v1/test_jira.py
@@ -26,3 +26,26 @@ def test_jira_comment_default(
                     headers=api_key_headers)
 
     assert r.status_code == 200
+
+
+@mock.patch('jira.JIRA.create_issue')
+def test_jira_issue_default(
+        mock_post, client: TestClient, api_key_headers: Dict[str, str]) -> None:
+    params = {
+        "project_key": "JRA",
+        "issue_type": "Bug",
+        "issue_summary": "Test"
+    }
+    _body = {
+        "body": "SAMPLE MESSAGE"
+    }
+    api_key_headers.update({"url": "https://jira.atlassian.com"})
+
+
+    r = client.post(f"{settings.API_V1_STR}/jira/new_issue",
+                    json=_body,
+                    params=params,
+                    allow_redirects=True,
+                    headers=api_key_headers)
+
+    assert r.status_code == 200
